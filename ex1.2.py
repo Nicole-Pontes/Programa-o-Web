8794a9c6065d23abb9d48cdeacf10ab33f418749
Nicole-Pontes <159967610+Nicole-Pontes@users.noreply.github.com>
2024-02-15 07:24:39 -0300

    ex1.2.py

--- ex1.2.py
+++ ex1.2.py
@@ -6,7 +6,7 @@ subtracao = nu1 - nu2
 divisao = nu1/nu2
 multiplicacao = nu1*nu2
 
-print ("O resultado da soma é: ", soma)
-print ("O resultado da subtração é: ", subtracao)
-print ("O resultado da divisão é: ", divisao)
-print ("O resultado da multiplicação é: ", multiplicacao)
+print (f"O resultado da soma é: {soma}")
+print (f"O resultado da subtração é: {subtracao}")
+print (f"O resultado da divisão é: {divisao}")
+print (f"O resultado da multiplicação é: {multiplicacao}")
