235f252ff3a8b1154569b64fa4db4c145dc98ad9
Nicole-Pontes <159967610+Nicole-Pontes@users.noreply.github.com>
2024-02-15 07:31:07 -0300

    Create ex5.1.py

---
+++ ex5.1.py
@@ -0,0 +1,5 @@
+nu1 = float(input("Digite o primeiro número a ser somado: "))
+nu2 = float(input("Digite o segundo número a ser somado: "))
+nu3 = float(input("Digite o terceiro número a ser somado: "))
+soma = nu1+nu2+nu3
+print(f"A soma é igual a {soma}")
